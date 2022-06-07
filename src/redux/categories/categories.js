99d85c09c834751b91128e6a379d773beed9dead
jmoseka <jamylam3@gmail.com>
2022-06-07 21:05:54 +0300

    Modify file

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,12 +1,12 @@
 // action types
 const CHECK_STATUS = 'bookstore_cms/bookReducer/checkStatus';
 
-export const checkStat = () => ({
+export const checkStatus = () => ({
   type: CHECK_STATUS,
 });
 
 const initialState = [];
-export default function statusReducer(state = initialState, action) {
+export default function checkStatusReducer(state = initialState, action) {
   switch (action.type) {
     case CHECK_STATUS:
       return 'Under Construction 🏗';
