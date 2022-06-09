190d6cf36a14ddaa3c1874c6d729ff5ce319a8d4
jmoseka <jamylam3@gmail.com>
2022-06-09 23:33:32 +0300

    Add fetchbook function

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -21,6 +21,23 @@ export const removeBook = (id) => ({
   id,
 });
 
+export const fetchBook = () => async (dispatch) => {
+  try {
+    const response = await axios.get(url);
+    const data = await response.data;
+    const books = [];
+    Object.entries(data).forEach((el) => {
+      books.push({
+        item_id: el[0],
+        ...el[1][0],
+      });
+    });
+    dispatch(getBook(books));
+  } catch (error) {
+    <h2>{error}</h2>;
+  }
+};
+
 export default function bookReducer(state = [], action) {
   switch (action.type) {
     case GET_BOOK:
